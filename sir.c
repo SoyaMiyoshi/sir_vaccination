5f301808ad5fd7dff682fc77ba9deec969cd3b92
Soya Miyoshi <miyoshisoya@gmail.com>
2020-06-24 20:29:41 +0900

    flag下げ

--- sir.c
+++ sir.c
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#define DEBUG_FLAG 1
+#define DEBUG_FLAG 0
 #define TIMESERIES_WATCHER 0
 #define DIFF 0.000001 // error は -15の オーダー
 
